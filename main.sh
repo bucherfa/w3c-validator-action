b1c7a491de6b2fe8bdabe880f051c46d9a373c40
Fabian Bucher <git@fabianbucher.de>
2021-01-13 22:44:59 +0100

    only output file name on error

--- main.sh
+++ main.sh
@@ -4,15 +4,15 @@ HTML_FILES=$(find . -type f -name '*.html')
 EXIT_CODE=0
 
 for FILE in $HTML_FILES; do
-  echo "File: $FILE"
   RESULT=$(curl --silent --header "Content-Type: text/html; charset=utf-8" \
     --data-binary @"$FILE" \
     https://validator.w3.org/nu/?out=text)
-  echo "$RESULT"
   if [[ $RESULT == *"There were errors."* ]]; then
     EXIT_CODE=1
+    echo "File: $FILE"
+    echo "$RESULT"
+    echo ""
   fi
-  echo ""
 done
 
 exit $EXIT_CODE
